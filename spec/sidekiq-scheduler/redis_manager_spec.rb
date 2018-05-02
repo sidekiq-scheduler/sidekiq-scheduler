e3559ee714cf3d9b4e9322d99d18bb153b2d2cbd
Andrea Dal Ponte <info@andreadalponte.com>
2018-05-02 15:45:51 +0200

    Failing spec using a half second interval

--- spec/sidekiq-scheduler/redis_manager_spec.rb
+++ spec/sidekiq-scheduler/redis_manager_spec.rb
@@ -308,7 +308,7 @@ describe SidekiqScheduler::RedisManager do
       it { is_expected.to be_falsey }
 
       context 'when registering the new instance with a different time' do
-        let(:time) { current_time + 1 }
+        let(:time) { current_time + 0.5 }
 
         it { is_expected.to be_truthy }
       end
