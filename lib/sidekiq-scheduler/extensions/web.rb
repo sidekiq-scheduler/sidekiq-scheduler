349eb73218bb9ea6409352c7b3813abfc51ff047
Marcelo Lauxen <marcelolauxen16@gmail.com>
2021-07-17 14:33:52 -0300

    Move css to its own file

--- lib/sidekiq-scheduler/extensions/web.rb
+++ lib/sidekiq-scheduler/extensions/web.rb
@@ -1,5 +1,11 @@
 require 'sidekiq/web' unless defined?(Sidekiq::Web)
 
+ASSETS_PATH = File.expand_path('../../../../web/assets', __dir__)
+
 Sidekiq::Web.register(SidekiqScheduler::Web)
 Sidekiq::Web.tabs['recurring_jobs'] = 'recurring-jobs'
-Sidekiq::Web.locales << File.expand_path(File.dirname(__FILE__) + '/../../../web/locales')
+Sidekiq::Web.locales << File.expand_path("#{File.dirname(__FILE__)}/../../../web/locales")
+Sidekiq::Web.use Rack::Static, urls: ['/stylesheets'],
+                               root: ASSETS_PATH,
+                               cascade: true,
+                               header_rules: [[:all, { 'Cache-Control' => 'public, max-age=86400' }]]
