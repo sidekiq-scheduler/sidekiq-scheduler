cc3c86d2c455c4972d2e1290e2ee58e6f513fa5f
Marcelo Lauxen <marcelolauxen16@gmail.com>
2022-03-30 08:18:04 -0300

    Require redis 4.2 and fix  deprecation warning

--- lib/sidekiq-scheduler/redis_manager.rb
+++ lib/sidekiq-scheduler/redis_manager.rb
@@ -96,14 +96,7 @@ module SidekiqScheduler
     #
     # @return [Boolean] true if the schedules key is set, false otherwise
     def self.schedule_exist?
-      Sidekiq.redis do |r|
-        case r.exists('schedules')
-        when true, 1
-          true
-        else
-          false
-        end
-      end
+      Sidekiq.redis { |r| r.exists?('schedules') }
     end
 
     # Returns all the schedule changes for a given time range.
