c3e2a763debae1afa2c581693f0fa46d56bad7cc
Jason-hou <houdl730@gmail.com>
2022-07-23 13:20:09 +0800

    rename stylesheets, it conflict with sidekiq rake asset

--- lib/sidekiq-scheduler/extensions/web.rb
+++ lib/sidekiq-scheduler/extensions/web.rb
@@ -7,7 +7,7 @@ Sidekiq::Web.tabs['recurring_jobs'] = 'recurring-jobs'
 Sidekiq::Web.locales << File.expand_path("#{File.dirname(__FILE__)}/../../../web/locales")
 
 if Sidekiq::VERSION >= '6.0.0'
-  Sidekiq::Web.use Rack::Static, urls: ['/stylesheets'],
+  Sidekiq::Web.use Rack::Static, urls: ['/stylesheets-scheduler'],
                                  root: ASSETS_PATH,
                                  cascade: true,
                                  header_rules: [[:all, { 'Cache-Control' => 'public, max-age=86400' }]]
