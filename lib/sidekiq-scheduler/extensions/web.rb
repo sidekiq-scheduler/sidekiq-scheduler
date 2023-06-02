a36871ccbffdcd5cb6ce9139d4f6eb05569ad26c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2023-06-03 07:37:04 +0900

    Remove code for Sidekiq < 6.0
    sidekiq-scheduler only supports Sidekiq 6.0 since #411.

--- lib/sidekiq-scheduler/extensions/web.rb
+++ lib/sidekiq-scheduler/extensions/web.rb
@@ -6,9 +6,7 @@ Sidekiq::Web.register(SidekiqScheduler::Web)
 Sidekiq::Web.tabs['recurring_jobs'] = 'recurring-jobs'
 Sidekiq::Web.locales << File.expand_path("#{File.dirname(__FILE__)}/../../../web/locales")
 
-if Sidekiq::VERSION >= '6.0.0'
-  Sidekiq::Web.use Rack::Static, urls: ['/stylesheets-scheduler'],
-                                 root: ASSETS_PATH,
-                                 cascade: true,
-                                 header_rules: [[:all, { 'cache-control' => 'public, max-age=86400' }]]
-end
+Sidekiq::Web.use Rack::Static, urls: ['/stylesheets-scheduler'],
+                               root: ASSETS_PATH,
+                               cascade: true,
+                               header_rules: [[:all, { 'cache-control' => 'public, max-age=86400' }]]
