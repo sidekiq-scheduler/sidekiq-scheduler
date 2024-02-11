3e94f52eb32dad96dbbad4e5b65c94586e6ef4e0
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2024-02-11 12:49:45 +0900

    Fix broken CI against Sidekiq 7.2
    The API of Redis commands via Sidekiq was changed at Sidekiq 7.2.
    This PR fixes to use the new style.
    
    Ref: https://github.com/sidekiq/sidekiq/issues/6083、 https://github.com/sidekiq/sidekiq/commit/3b8a3c3ea29b63b21e01010f630c936d25265712

--- spec/support/initialization/redis.rb
+++ spec/support/initialization/redis.rb
@@ -2,6 +2,10 @@ require 'sidekiq/redis_connection'
 
 RSpec.configure do |config|
   config.before do
-    Sidekiq.redis { |r| r.flushdb(async: true) }
+    if Sidekiq::VERSION >= '7.2'
+      Sidekiq.redis { |r| r.flushdb('async') }
+    else
+      Sidekiq.redis { |r| r.flushdb(async: true) }
+    end
   end
 end
