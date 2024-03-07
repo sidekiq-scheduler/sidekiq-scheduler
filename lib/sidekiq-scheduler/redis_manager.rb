1e8d186d5f856d860e92fd89a68e4647730c6da8
Aaron Frase <afrase91@gmail.com>
2024-03-07 11:21:20 -0500

    Replace Redis pipeline with multi in register_job_instance.

--- lib/sidekiq-scheduler/redis_manager.rb
+++ lib/sidekiq-scheduler/redis_manager.rb
@@ -130,9 +130,9 @@ module SidekiqScheduler
     def self.register_job_instance(job_name, time)
       job_key = pushed_job_key(job_name)
       registered, _ = Sidekiq.redis do |r|
-        r.pipelined do |pipeline|
-          pipeline.zadd(job_key, time.to_i, time.to_i)
-          pipeline.expire(job_key, REGISTERED_JOBS_THRESHOLD_IN_SECONDS)
+        r.multi do |m|
+          m.zadd(job_key, time.to_i, time.to_i)
+          m.expire(job_key, REGISTERED_JOBS_THRESHOLD_IN_SECONDS)
         end
       end
 
