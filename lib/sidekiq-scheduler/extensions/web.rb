c63003752ac9a33cc7fa76bc3b84e01af2f25dd3
Tony Drake <t27duck@gmail.com>
2024-07-02 17:24:32 -0400

    Use new web ui registration and stylesheet helpers
    Sidekiq 7.3.0 and later added changes to how the web UI for plugins work.
    
    - The registration has a first-class API to register a plugin.
      - See: https://github.com/sidekiq/sidekiq/blob/main/examples/webui-ext/lib/sidekiq-redis_info/web.rb#L20
      - The old registration path is still allowed, but will be removed in Sidekiq 8.0.
    - New stylesheet and script helpers were added to allow linking to scripts and stylesheets for the plugin. They add nouce attributes to protect against XSS attacks.
      - See: https://github.com/sidekiq/sidekiq/blob/main/examples/webui-ext/readme.md
      - Sidekiq 8.0 and later will require nouce attributes for all of Sidekiq web.
    
    To make this gem forwards compatible with 8.0...
    
    - Added a new constant to check for Sidekiq 7.3 and later.
    - For current and future Sidekiq releases, we'll use the new `style_tag` helper to output the `<link>` tag. Older Sidekiq versions will continue to use the hand-crafted tag as it stands today.
    - For current and future Sidekiq releases, we'll use the newer registration API for the web UI.

--- lib/sidekiq-scheduler/extensions/web.rb
+++ lib/sidekiq-scheduler/extensions/web.rb
@@ -1,12 +1,27 @@
 require 'sidekiq/web' unless defined?(Sidekiq::Web)
 
-ASSETS_PATH = File.expand_path('../../../web/assets', __dir__)
+if SidekiqScheduler::SidekiqAdapter::SIDEKIQ_GTE_7_3_0
 
-Sidekiq::Web.register(SidekiqScheduler::Web)
-Sidekiq::Web.tabs['recurring_jobs'] = 'recurring-jobs'
-Sidekiq::Web.locales << File.expand_path("#{File.dirname(__FILE__)}/../../../web/locales")
+  # Locale and asset cache is configured in `.regiester`
+  Sidekiq::Web.register(SidekiqScheduler::Web,
+    name: "recurring_jobs",
+    tab: ["Recurring Jobs"],
+    index: ["recurring-jobs"],
+    root_dir: File.expand_path("../../../web", File.dirname(__FILE__)),
+    asset_paths: ["stylesheets-scheduler"]) do |app|
+    # add middleware or additional settings here
+  end
 
-Sidekiq::Web.use Rack::Static, urls: ['/stylesheets-scheduler'],
-                               root: ASSETS_PATH,
-                               cascade: true,
-                               header_rules: [[:all, { 'cache-control' => 'private, max-age=86400' }]]
+else
+
+  ASSETS_PATH = File.expand_path('../../../web/assets', __dir__)
+
+  Sidekiq::Web.register(SidekiqScheduler::Web)
+  Sidekiq::Web.tabs['recurring_jobs'] = 'recurring-jobs'
+  Sidekiq::Web.locales << File.expand_path("#{File.dirname(__FILE__)}/../../../web/locales")
+
+  Sidekiq::Web.use Rack::Static, urls: ['/stylesheets-scheduler'],
+                                 root: ASSETS_PATH,
+                                 cascade: true,
+                                 header_rules: [[:all, { 'cache-control' => 'private, max-age=86400' }]]
+end
