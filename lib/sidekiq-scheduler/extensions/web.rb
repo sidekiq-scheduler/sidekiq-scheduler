dc44da35297c6cb72540cb920ab68fecdce5be70
zzak <zzak@hey.com>
2025-05-21 15:36:11 +0900

    Sidekiq::Web v7.3.0-v7.3.6 does not have configure
    See: sidekiq/sidekiq@cf7a879
    
    ```
    Original Error: NoMethodError vendor/bundle/ruby/3.3.0/gems/sidekiq-scheduler-6.0.0/lib/sidekiq-scheduler/extensions/web.rb:4:in `<main>': undefined method `configure' for class Sidekiq::Web (NoMethodError)
    
    Sidekiq::Web.configure do |cfg|
                ^^^^^^^^^^
            from sidekiq-scheduler-6.0.0/lib/sidekiq-scheduler/web.rb:59:in `require_relative'
            from sidekiq-scheduler-6.0.0/lib/sidekiq-scheduler/web.rb:59:in `<main>'
    ```
    
    This fixes for Sidekiq::Web < v7.3.7

--- lib/sidekiq-scheduler/extensions/web.rb
+++ lib/sidekiq-scheduler/extensions/web.rb
@@ -1,13 +1,22 @@
 require 'sidekiq/web' unless defined?(Sidekiq::Web)
 
 # Locale and asset cache is configured in `cfg.register`
-Sidekiq::Web.configure do |cfg|
-  cfg.register(SidekiqScheduler::Web,
-    name: "recurring_jobs",
-    tab: ["Recurring Jobs"],
-    index: ["recurring-jobs"],
-    root_dir: File.expand_path("../../../web", File.dirname(__FILE__)),
-    asset_paths: ["stylesheets-scheduler"]) do |app|
+args = {
+  name: "recurring_jobs",
+  tab: ["Recurring Jobs"],
+  index: ["recurring-jobs"],
+  root_dir: File.expand_path("../../../web", File.dirname(__FILE__)),
+  asset_paths: ["stylesheets-scheduler"]
+}
+
+if SidekiqScheduler::SidekiqAdapter::SIDEKIQ_GTE_8_0_0
+  Sidekiq::Web.configure do |cfg|
+    cfg.register(SidekiqScheduler::Web, **args) do |app|
+      # add middleware or additional settings here
+    end
+  end
+else
+  Sidekiq::Web.register(SidekiqScheduler::Web, **args) do |app|
     # add middleware or additional settings here
   end
 end
